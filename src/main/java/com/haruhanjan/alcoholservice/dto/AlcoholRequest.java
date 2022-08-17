2d32f1c598524b64034a76fd6e6a613a5f1bec94
SEONMl <sm000117@naver.com>
2022-08-17 14:25:53 +0900

    feat: #1 modify CRUD code

--- src/main/java/com/haruhanjan/alcoholservice/dto/AlcoholRequest.java
+++ src/main/java/com/haruhanjan/alcoholservice/dto/AlcoholRequest.java
@@ -1,25 +1,36 @@
 package com.haruhanjan.alcoholservice.dto;
 
 import com.haruhanjan.alcoholservice.entity.Alcohol;
-import lombok.Builder;
+import com.haruhanjan.alcoholservice.entity.AlcoholType;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 @Setter
 public class AlcoholRequest {
 
     @Getter
     private Long id;
-
+    private String name;
+    private float originVolume;
+    private int volume; // 도수*10
+    private String madeFrom; // 원산지
+    private String seller;
+    private String alcoholType;
+    private LocalDate productDate;
+    @Getter
     private int price;
-    private String classification;
 
     public Alcohol toEntity(){
         return Alcohol.builder()
+                .name(name)
                 .price(price)
-                .classification(classification)
+                .volume((int) (originVolume*10))
+                .madeFrom(madeFrom)
+                .seller(seller)
+                .alcoholType(AlcoholType.valueOf(alcoholType))
+                .productDate(productDate)
                 .build();
     }
 }
