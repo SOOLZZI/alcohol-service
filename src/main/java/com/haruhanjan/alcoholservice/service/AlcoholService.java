5c41b26838b8ff5c96d3b1d2e49be3571e1853d4
SEONMl <sm000117@naver.com>
2022-08-19 16:44:00 +0900

    feat: DDD 패턴 적용

--- src/main/java/com/haruhanjan/alcoholservice/service/AlcoholService.java
+++ src/main/java/com/haruhanjan/alcoholservice/service/AlcoholService.java
@@ -1,19 +1,16 @@
 package com.haruhanjan.alcoholservice.service;
 
-import com.haruhanjan.alcoholservice.dto.CreateAlcoholRequestDTO;
 import com.haruhanjan.alcoholservice.dto.AlcoholResponseDTO;
+import com.haruhanjan.alcoholservice.dto.CreateAlcoholRequestDTO;
 import com.haruhanjan.alcoholservice.dto.ModifyAlcoholDTO;
 import com.haruhanjan.alcoholservice.entity.Alcohol;
-import com.haruhanjan.alcoholservice.entity.AlcoholType;
 import com.haruhanjan.alcoholservice.repository.AlcoholRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityNotFoundException;
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -31,22 +28,13 @@ public class AlcoholService {
     @Transactional
     public void modify(Long id, ModifyAlcoholDTO dto) {
         Alcohol target = alcoholRepository.findById(id).orElseThrow(EntityNotFoundException::new);
-
-        Optional.ofNullable(dto.getPrice()).ifPresent(target::setPrice);
-        Optional.ofNullable(dto.getAlcoholType()).ifPresent(param -> target.setAlcoholType(AlcoholType.valueOf(param)));
-        Optional.ofNullable(dto.getName()).ifPresent(param -> target.setName(param));
-        Optional.ofNullable(dto.getProductDate()).ifPresent(param -> target.setProductDate(param));
-        Optional.ofNullable(dto.getOriginVolume()).ifPresent(param -> target.setVolume((int) (param*10)));
-        Optional.ofNullable(dto.getSeller()).ifPresent(param -> target.setSeller(param));
-        Optional.ofNullable(dto.getMadeFrom()).ifPresent(param -> target.setMadeFrom(param));
-
-        target.setUpdatedAt();
+        target.modify(dto);
     }
 
 
     @Transactional
     public void deleteById(Long id) {
-        Alcohol target =alcoholRepository.findById(id).orElseThrow(EntityNotFoundException::new);
+        Alcohol target = alcoholRepository.findById(id).orElseThrow(EntityNotFoundException::new);
         target.delete();
     }
 
