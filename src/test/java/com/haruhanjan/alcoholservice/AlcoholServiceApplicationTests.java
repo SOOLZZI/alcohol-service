98ca79d0a07268106e8d431a8efa479e04467633
Hyeonjun Park <phjppo0918@gmail.com>
2022-08-31 12:30:48 +0900

    test: ApplicationTests 추가

--- src/test/java/com/haruhanjan/alcoholservice/AlcoholServiceApplicationTests.java
+++ src/test/java/com/haruhanjan/alcoholservice/AlcoholServiceApplicationTests.java
@@ -1,13 +1,17 @@
 package com.haruhanjan.alcoholservice;
 
+
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
-class AlcoholServiceApplicationTests {
-
+@Slf4j
+@ExtendWith(SpringExtension.class)
+public class AlcoholServiceApplicationTests {
     @Test
     void contextLoads() {
+        log.info("test start");
     }
-
 }
+
